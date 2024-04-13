fbdeada20bdada655c774fce255c5d1375dc575b
Pedro Gonzalez <orion@Pedros-MacBook-Pro.local>
2024-04-13 11:42:49 -0500

    added the belongsToMany relationships with Product and Tag.

--- models/index.js
+++ models/index.js
@@ -10,10 +10,20 @@ Product.belongsTo(category, {
   onDelete: "CASCADE"
 })
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE"
+})
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  foreignKey: "product_id",
+  through: ProductTag
+})
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product,{
+  foreignKey: "tag_id",
+  through: ProductTag
+})
 
 module.exports = {
   Product,
